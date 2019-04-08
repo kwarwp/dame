96792c98a948f374ecf5224ed220736929e1b818
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-08 08:18:07 -0300

    Created grace/__log__.py at Timestamp: 2019-04-08 11:18:07

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Apr 08 2019 08:18:05.433 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 27
+  STYLE = dict(with=900,height="700px")
+                ^
+SyntaxError: invalid syntax
+'''},
