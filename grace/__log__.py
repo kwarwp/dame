1a94daed6d716488aae474742eae1831620db774
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-08 08:19:08 -0300

    Saved grace/__log__.py at Timestamp: 2019-04-08 11:19:08

--- grace/__log__.py
+++ grace/__log__.py
@@ -6,3 +6,10 @@
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Apr 08 2019 08:19:08.48 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 27
+  STYLE.update(dict(with=900,height="700px"))
+                     ^
+SyntaxError: invalid syntax
+'''},
