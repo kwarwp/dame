8ce5a356dcbf11d724b9334e6b53ec9842acba72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-09 15:31:19 -0300

    Saved danae/main.py at Timestamp: 2019-04-09 18:31:19

--- danae/main.py
+++ danae/main.py
@@ -3,7 +3,7 @@ from grace.main import main as grace
 from _spy.vitollino.main import Codigo
 DNA = "https://i.imgur.com/tEtZk2X.jpg"
 
-TEXTO = """<div style="width:100%; text-align: center;">
+TEXTO = ["""<div style="width:100%; text-align: center;">
 <H2>DIÁRIO DE PESQUISA</H2>
 <br>
 <img src="https://i.imgur.com/tEtZk2X.jpg" width=200 />
@@ -15,7 +15,34 @@ um crime.<br/>
 Preciso que você encontre as
 outras páginas para podermos
 solucionar este mistério
+""",
+"""<div style="width:100%; text-align: center;">
+<H2>Página 1</H2>
+<br>
+<img src="https://i.imgur.com/tEtZk2X.jpg" width=200 />
+</div>
+<br/><br/>
+Se você encontrou este livro
+é porque nesta casa aconteceu
+um crime.<br/>
+Preciso que você encontre as
+outras páginas para podermos
+solucionar este mistério
+""",
+"""<div style="width:100%; text-align: center;">
+<H2>Página 2</H2>
+<br>
+<img src="https://i.imgur.com/tEtZk2X.jpg" width=200 />
+</div>
+<br/><br/>
+Se você encontrou este livro
+é porque nesta casa aconteceu
+um crime.<br/>
+Preciso que você encontre as
+outras páginas para podermos
+solucionar este mistério
 """
+]
 TEXTO1 = """'''Se você encontrou este livro
 é porque nesta casa aconteceu
 um crime.
@@ -23,6 +50,15 @@ um crime.
 
 class Livro:
     def __init__(self, casa):
+        self.pagina_atual = 0
+        class PaginaAnterior:
+            def vai(self, *_):
+                self.pagina_atual -= 1
+                self.texto.topo.html = TEXTO[self.pagina_atual]
+        class PaginaPosterior:
+            def vai(self, *_):
+                self.pagina_atual += 1
+                self.texto.topo.html = TEXTO[self.pagina_atual]
         self.clica_livro = lambda: None
         self.onde = None
         _livro_fechado = "https://i.imgur.com/ty2fWuE.gif"
@@ -31,15 +67,11 @@ class Livro:
         self.casa.vit.i.inicia()
         livro = self.casa.vit.a(_livro_fechado, "caderno de notas",
             style=dict(left=280, top=500, width=60, height="60px"))
-        cena_livro = self.casa.vit.c(_livro_aberto, self, self)
+        cena_livro = self.casa.vit.c(_livro_aberto, PaginaAnterior(), PaginaPosterior())
         livro.entra(self.casa.sala.B.leste)
         self.texto = texto = Codigo(
              codigo=TEXTO1, topo=TEXTO,
              vai=self.pagina, style=dict(left=440, top=20, width=380))
-        texto.entra(cena_livro)
-        self.texto1 = texto1 = Codigo(
-             codigo=TEXTO1, topo=TEXTO,
-             vai=lambda *_: texto.vai, style=dict(left=40, top=20, width=380))
         texto.entra(cena_livro)
         def abre_livro(*_):
             self.clica_livro = lambda: self.vai()
