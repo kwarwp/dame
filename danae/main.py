3742f3e86925a5012132796ce972e98339f049e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-09 11:42:18 -0300

    Created danae/main.py at Timestamp: 2019-04-09 14:42:17

---
+++ danae/main.py
@@ -0,0 +1,15 @@
+# dame.danae.main.py
+from grace.main import main as grace
+
+
+class Livro
+    def __init__(self, casa):
+        self.casa = casa()
+
+
+def main(bry=grace):
+    return Livro(bry)
+
+
+if __name__ == '__main__':
+    main(grace)
