ff9d1a4605885b9b07a08d5036934dec3d1aec72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-09 11:44:22 -0300

    Created danae/__log__.py at Timestamp: 2019-04-09 14:44:22

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Apr 09 2019 11:44:20.645 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  class Livro
+              ^
+SyntaxError: invalid syntax
+'''},
