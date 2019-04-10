88db2df1858101cdfd4c84b18898936cbf34487f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-09 23:00:29 -0300

    Saved danae/__log__.py at Timestamp: 2019-04-10 02:00:28

--- danae/__log__.py
+++ danae/__log__.py
@@ -115,3 +115,24 @@ Traceback (most recent call last):
     porta.meio = self
 NameError: name 'porta' is not defined
 '''},
+{'date': 'Tue Apr 09 2019 23:00:27.382 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''sala_do_jogo.A.norte <_spy.vitollino.main.Sala object> <_spy.vitollino.main.Cena object> https://i.imgur.com/aLEjWgB.png
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 149
+    main(grace)
+  module <module> line 145
+    return Livro(bry)
+  module <module> line 125
+    pagina = Pagina(self.casa.vit, self.pagina)
+TypeError: __init__() takes 2 positional arguments but more were given
+'''},
