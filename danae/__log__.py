bfada00adc12c5b7b1ed9c5fdd04aa0bf067a964
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-09 23:25:40 -0300

    Saved danae/__log__.py at Timestamp: 2019-04-10 02:25:39

--- danae/__log__.py
+++ danae/__log__.py
@@ -143,3 +143,24 @@ TypeError: __init__() takes 2 positional arguments but more were given
                                                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Apr 09 2019 23:25:38.656 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''sala_do_jogo.A.norte <_spy.vitollino.main.Sala object> <_spy.vitollino.main.Cena object> https://i.imgur.com/aLEjWgB.png
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 149
+    main(grace)
+  module <module> line 145
+    return Livro(bry)
+  module <module> line 90
+    self.texto = texto = Codigo(
+AttributeError: 'Livro' object has no attribute 'pagina'
+'''},
