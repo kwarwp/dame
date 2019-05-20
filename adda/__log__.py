a4a7881742bf1d2a17070f16cb5c46c08ef9a1f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:59:17 -0300

    Saved adda/__log__.py at Timestamp: 2019-05-20 13:59:17

--- adda/__log__.py
+++ adda/__log__.py
@@ -291,3 +291,21 @@ NameError: name 'paisagem' is not defined
     morcego.entra (paisagem)
 NameError: name 'paisagem' is not defined
 '''},
+{'date': 'Mon May 20 2019 10:59:35.122 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    juntarimagem()
+  module <module> line 15
+    falamorcego= Texto(paisagem, "eu vou te pegar pinguim")
+NameError: name 'paisagem' is not defined
+'''},
