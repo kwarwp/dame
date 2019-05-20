57611ed986ab4aaf2c589c5b6616c40c9f9c33c8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:24:48 -0300

    Saved stacy/__log__.py at Timestamp: 2019-05-20 13:24:48

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -6,3 +6,10 @@
                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 20 2019 10:24:47.212 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  bart_Simpson = Elemento (img = Bart_Simpson, tit"Oi, eu sou o Bart!", Style = dict(left=150, top=60, weith=60, heith=200))
+                                                   ^
+SyntaxError: invalid syntax
+'''},
