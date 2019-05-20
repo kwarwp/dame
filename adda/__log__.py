e5513f4b29ae24ed40061d7f71cf9fd0c49e025d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:55:42 -0300

    Saved adda/__log__.py at Timestamp: 2019-05-20 13:55:40

--- adda/__log__.py
+++ adda/__log__.py
@@ -183,3 +183,21 @@ UnboundLocalError: local variable 'verde' referenced before assignment
     agua = Cena(img = agua)
 UnboundLocalError: local variable 'agua' referenced before assignment
 '''},
+{'date': 'Mon May 20 2019 10:55:53.722 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    juntarimagem()
+  module <module> line 13
+    morcego = Elemento (img = morcego , tit="BATMAN", style= dict (left=150,top=160,width=60,height=200))
+UnboundLocalError: local variable 'morcego' referenced before assignment
+'''},
