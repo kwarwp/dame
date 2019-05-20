4442b7b6b5e0a6f01950fdc02f0df727549d9261
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:27:21 -0300

    Created morgan/__log__.py at Timestamp: 2019-05-20 13:27:21

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon May 20 2019 10:27:14.24 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  THOR = Elemento (img =Thor,tit "Capitão",Style =dict (left=150,top=160,width=60,height=200))
+                                  ^
+SyntaxError: invalid syntax
+'''},
