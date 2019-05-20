4df9ea67da8fb1599d714784eccefec0e09a6512
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:28:41 -0300

    Saved morgan/__log__.py at Timestamp: 2019-05-20 13:28:41

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -6,3 +6,10 @@
                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 20 2019 10:28:40.545 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  Tiana = Elemento (img =Tiana,tit "Tiana",Style=dict(left=150,top=160,width=60,height=200))
+                                    ^
+SyntaxError: invalid syntax
+'''},
