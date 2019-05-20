ed6809e1beb0f599356a96b2387250128a6423d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:31:21 -0300

    Saved morgan/main.py at Timestamp: 2019-05-20 13:31:20

--- morgan/main.py
+++ morgan/main.py
@@ -5,19 +5,18 @@ THOR = "https://pngmafia.net/image/2019/01/Thor-10-min.png"
 CAMPO = "https://png.pngtree.com/element_origin_min_pic/16/09/08/2157d1695243d31.jpg"
 TIANA = "https://www.pngkit.com/png/detail/168-1689175_princess-tiana-roommates-disney-princess-the-frog-princess.png"
 def funcoes ( ):
-    Paisagem = Cena (img =Paisagem)
-    Campo = Cena (img =Campo)
+    Paisagem = Cena (img =PAISAGEM)
+    Campo = Cena (img =CAMPO)
     Campo.direita = Paisagem
     Paisagem.esquerda = Campo
-    THOR = Elemento (img =Thor,tit "Capitão",Style =dict (left=150,top=160,width=60,height=200))
+    Thor = Elemento (img =THOR,tit= "Capitão",style =dict (left=150,top=160,width=60,height=200))
     Thor.entra(Paisagem)
     eThor=Texto(Paisagem,"Olá, vim de Asgard e caí nessa praia. Sou Thor, Deus do Trovão e filho de Odin")
     Thor=eThor.vai
-    Tiana = Elemento (img =Tiana,tit "Tiana",Style=dict(left=150,top=160,width=60,height=200))
+    Tiana = Elemento (img =TIANA,tit= "Tiana",style=dict(left=150,top=160,width=60,height=200))
     Tiana.entra(Campo)
     eTiana=Texto(Campo,"Oi, me chamo Tiana e estou a procura do meu príncipe encantado.Você pode me ajudar?")
-    Tiana=eTiana.vai
+    Tiana=eTiana.vai()
     
     Campo.vai()
-    funcoes()
-    
+funcoes()
