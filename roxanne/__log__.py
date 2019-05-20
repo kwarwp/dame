27ef3d4dc013e011e066c6ac2faf70a0918fc23a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:24:02 -0300

    Saved roxanne/__log__.py at Timestamp: 2019-05-20 13:24:01

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -6,3 +6,10 @@
                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 20 2019 10:24:00.524 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  tony=Elemento(img = HOMEM_DE_FERRO , tit "TONY STARK" , style=dict (left= 150,top=160,width=60,height=200))
+                                            ^
+SyntaxError: invalid syntax
+'''},
