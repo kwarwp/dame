e9c8acffdfd2d3a66c250dbefd84f6a36de0878b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:51:36 -0300

    Saved adda/__log__.py at Timestamp: 2019-05-20 13:51:36

--- adda/__log__.py
+++ adda/__log__.py
@@ -104,3 +104,10 @@ SyntaxError: invalid syntax
                                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 20 2019 10:51:54.795 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  feio = (img = feio , tit = "PINGUIM", style=dict (left=150,top=160,width=60,height=200))
+                                                                                          ^
+SyntaxError: invalid syntax
+'''},
