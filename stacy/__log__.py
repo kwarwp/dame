3a2c7647af792446ccd8dba31a23f0c88bc25a00
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:31:11 -0300

    Saved stacy/__log__.py at Timestamp: 2019-05-20 13:31:11

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -43,3 +43,19 @@ SyntaxError: invalid syntax
     Furia.da.Noite = "https://i.pinimg.com/originals/1e/45/c7/1e45c7fd135d3b8fed8f18a33447a019.png"
 NameError: name 'Furia' is not defined
 '''},
+{'date': 'Mon May 20 2019 10:31:09.593 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    Furia.da.Noite = "https://i.pinimg.com/originals/1e/45/c7/1e45c7fd135d3b8fed8f18a33447a019.png"
+NameError: name 'Furia' is not defined
+'''},
