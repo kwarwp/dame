ba8f81984665b2d048cc723a4034d539c253f7e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:23:36 -0300

    Created stacy/__log__.py at Timestamp: 2019-05-20 13:23:35

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon May 20 2019 10:23:32.544 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  Bart_Simpson = Elemento (img = Bart Simpson, tit"Oi, eu sou o Bart!", Style = dict(left=150, top=60, weith=60, heith=200))
+                                       ^
+SyntaxError: invalid syntax
+'''},
