337146965848312129c06ca67a9b508f732279b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:49:48 -0300

    Saved adda/__log__.py at Timestamp: 2019-05-20 13:49:47

--- adda/__log__.py
+++ adda/__log__.py
@@ -83,3 +83,10 @@ SyntaxError: invalid syntax
                                                                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 20 2019 10:50:06.435 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  falamorcego= Texto(paisagem"eu vou te pegar pinguim"
+                              ^
+SyntaxError: invalid syntax
+'''},
