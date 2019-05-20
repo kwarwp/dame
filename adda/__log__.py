64bfe7313a3e350a4c1612ec46375d3c3ff4b134
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:44:39 -0300

    Saved adda/__log__.py at Timestamp: 2019-05-20 13:44:38

--- adda/__log__.py
+++ adda/__log__.py
@@ -41,3 +41,10 @@ SyntaxError: invalid syntax
                                                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 20 2019 10:44:57.59 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  BATMAN = (img = BATMAN , tit = "BATMAN", style=dict (left=150,top=160,width=60,height=200))
+                                                                                             ^
+SyntaxError: invalid syntax
+'''},
