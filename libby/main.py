5e3695014d78b53731ae766cc39e11b857b00de4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:45:55 -0300

    Saved libby/main.py at Timestamp: 2019-05-20 13:45:54

--- libby/main.py
+++ libby/main.py
@@ -1,6 +1,6 @@
 # dame.libby.main.py
 from _spy.vitollino.main import Cena,Texto,Elemento
-CAPITA_MARVEL = "http://i0.wp.com/multversogeek.com.br/wp-content/uploads/2017/04/captainmarvel.jpg"
+CAPITA_MARVEL = "https://upload.wikimedia.org/wikipedia/en/e/e0/Cover_of_Captain_Marvel_1_-_2013.jpg"
 ESPACO = "https://ciencianautas.com/wp-content/uploads/2016/12/wallpapers_kate_beckinsale_planet_earth_antarctica_hd_2560x1024_interesting_places.jpg"
 HOMEM_DE_FERRO = "https://cache.popcultcha.com.au/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/h/o/hotmms528d30-avengers-endgame-iron-man-mark-85-die-cast-hot-toys-action-figure-10.1554444212.jpg"
 CIDADE = "http://danibrasil.com/wp-content/uploads/2015/02/paris-night.jpg"
