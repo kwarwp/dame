480b3de70af30b9bc653b4212df4cc0004f03187
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:44:26 -0300

    Saved libby/main.py at Timestamp: 2019-05-20 13:44:26

--- libby/main.py
+++ libby/main.py
@@ -1,6 +1,5 @@
 # dame.libby.main.py
 from _spy.vitollino.main import Cena,Texto,Elemento
-from _spy.vitolli.main import invetario as inv 
 CAPITA_MARVEL = "http://i0.wp.com/multversogeek.com.br/wp-content/uploads/2017/04/captainmarvel.jpg"
 ESPACO = "https://ciencianautas.com/wp-content/uploads/2016/12/wallpapers_kate_beckinsale_planet_earth_antarctica_hd_2560x1024_interesting_places.jpg"
 HOMEM_DE_FERRO = "https://cache.popcultcha.com.au/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/h/o/hotmms528d30-avengers-endgame-iron-man-mark-85-die-cast-hot-toys-action-figure-10.1554444212.jpg"
@@ -8,11 +7,12 @@ CIDADE = "http://danibrasil.com/wp-content/uploads/2015/02/paris-night.jpg"
 def oi ():
     galaxia = Cena(img = ESPACO)
     paris = Cena(img = CIDADE)
-    galaxia.esquerda = galaxia
-    paris.direita = paris 
+    galaxia.esquerda = paris
+    paris.direita = galaxia 
     danvers=Elemento(img = CAPITA_MARVEL , tit="CAROL DANVERS" , style=dict (left= 150,top=160,widht=60,height=200))
     danvers.entra(galaxia)
     faladanvers= Texto(galaxia, "Eu sou a Capita Marvel")
-    danvers = faladanvers.vai()
+    faladanvers.vai()
+    galaxia.vai()
     
 oi()
