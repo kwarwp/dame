a60b72f54b16c6f01e802f66a172efe7b10f2863
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:46:29 -0300

    Saved stacy/__log__.py at Timestamp: 2019-05-20 13:46:28

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -160,3 +160,23 @@ Traceback (most recent call last):
   module <module> line 3
     from _spy.vitollino.main import inventario as inv
 '''},
+{'date': 'Mon May 20 2019 10:46:24.610 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 21
+    oi()
+  module <module> line 20
+    falaBart.vai()
+  module _spy.vitollino.main line 994
+    self.cena.elt <= Popup.POP.popup
+AttributeError: 'str' object has no attribute 'elt'
+'''},
