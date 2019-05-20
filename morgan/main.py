d90bd5af15415fe32f76911624d3701534a320b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:45:03 -0300

    Saved morgan/main.py at Timestamp: 2019-05-20 13:45:02

--- morgan/main.py
+++ morgan/main.py
@@ -3,7 +3,7 @@ from _ spy.vitollino.main import Inventario as inv
 PAISAGEM = "https://pngimage.net/wp-content/uploads/2018/06/fundo-paisagem-png.png/"
 THOR = "https://pngmafia.net/image/2019/01/Thor-10-min.png"
 CAMPO = "https://png.pngtree.com/element_origin_min_pic/16/09/08/2157d1695243d31.jpg"
-TIANA = "https://www.pngkit.com/png/detail/168-1689175_princess-tiana-roommates-disney-princess-the-frog-princess.png"
+TIANA = "https://upload.wikimedia.org/wikipedia/ru/5/55/Princess-tiana-disney.jpg"
 def funcoes ( ):
     Paisagem = Cena (img =PAISAGEM)
     Campo = Cena (img =CAMPO)
