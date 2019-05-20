ab14c21d73ac01ecb0af153e459d772aa8fbcd99
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:31:14 -0300

    Saved adda/__log__.py at Timestamp: 2019-05-20 13:31:13

--- adda/__log__.py
+++ adda/__log__.py
@@ -6,3 +6,10 @@
                                                     ^
 SyntaxError: trailing comma not allowed without surrounding parentheses
 '''},
+{'date': 'Mon May 20 2019 10:31:28.612 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy. vitollino.main import Cena, Texto, Ele...
+                                                    ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
