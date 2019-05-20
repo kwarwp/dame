65de288f9a31672139f6434f21bd624abc31bb38
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-20 10:15:27 -0300

    Created angie/__log__.py at Timestamp: 2019-05-20 13:15:26

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon May 20 2019 10:15:22.218 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  class Livro
+              ^
+SyntaxError: invalid syntax
+'''},
