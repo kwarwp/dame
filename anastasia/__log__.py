d22ad69c4dacecaf92607d24a37cd1e35d4ec23c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-27 11:08:36 -0300

    Saved anastasia/__log__.py at Timestamp: 2019-05-27 14:08:35

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -51,3 +51,19 @@ TypeError: __init__ missing 1 positional argument: 'jogo'
     Livro()
 TypeError: __init__ missing 1 positional argument: 'jogo'
 '''},
+{'date': 'Tue Oct 20 2009 02:06:22.584 GMt-0200 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    STYLE["width"] = 900
+AttributeError: 'module' object has no attribute '__setitem__'
+'''},
