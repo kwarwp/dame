53c650950aeddc4e2af8539afb2be1a388fec29f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-27 10:05:23 -0300

    Saved anastasia/main.py at Timestamp: 2019-05-27 13:05:22

--- anastasia/main.py
+++ anastasia/main.py
@@ -2,3 +2,13 @@
 from spy.vittolino.main import Cena
 LIVRO = "https://i.imgur.com/sI177hV.jpg"
 PAGINA = "https://i.imgur.com/YqUFpx4.jpg"
+.
+
+class Livro:
+    def __init__(self, jogo, pagina = None):
+        self.livro = Cena(LIVRO)
+        self.livro.vai()
+        
+        
+if __name__ == "__main__":
+    Livro()
