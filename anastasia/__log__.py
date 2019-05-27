495c4e8801176881868f24e7978d60520c5fb16a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-27 10:40:50 -0300

    Saved anastasia/__log__.py at Timestamp: 2019-05-27 13:40:50

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -35,3 +35,19 @@ AttributeError: 'NoneType' object has no attribute 'codigo'
     Livro()
 TypeError: __init__ missing 1 positional argument: 'jogo'
 '''},
+{'date': 'Mon May 27 2019 10:40:48.228 GMt-0300 (GMT-03:00) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    Livro()
+TypeError: __init__ missing 1 positional argument: 'jogo'
+'''},
