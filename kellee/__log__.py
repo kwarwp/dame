9745ca001611526d1f1dd30d6131e0143a83e6a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-25 16:46:38 -0300

    Saved kellee/__log__.py at Timestamp: 2019-06-25 19:46:38

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -6,3 +6,10 @@
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Jun 25 2019 16:46:32.473 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  <blockquote class="imgur-embed-pub" lang="en" data-id="a/5AwH2aG"><a href="//imgur.com/a/5AwH2aG">Python Sudeste</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+   ^
+SyntaxError: invalid syntax
+'''},
