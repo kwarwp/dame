499fce3c3c8541dc953c0876980ea9f2c601cd18
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-07-02 18:36:29 -0300

    Created amilase/main.py at Timestamp: 2019-07-02 21:36:28

---
+++ amilase/main.py
@@ -0,0 +1,19 @@
+# dame.amilase.main.py
+from elemento.main import Elemento
+from _spy.vitollino import INVENTARIO as inv
+AMILASE = "https://i.imgur.com/6sPzHr4.png"
+COLA = "https://i.imgur.com/fUmF8X9.png"
+
+
+class Amilase:
+    def __init__(self, vit):
+        self.vit = vit
+        self.porta = vit.sala.C.sul
+        self.aviso = vit.n(porta, TRANCA)
+        porta.meio = self
+        self.amilase = Elemento(AMILASE, tit="Amilase Sintética", drag=True)
+        self.cola = Elemento(COLA, tit="Cola Glicosada", drop={"Amilase Sintética": self.abre})
+        inv.bota(self.amilase)
+        
+    def abre(self, *_):
+        self.porta.meio =  self.vit.sala.D.sul       
