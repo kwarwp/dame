874d49bf355dd26765c522d498162308da65b4fb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-07-02 18:51:50 -0300

    Saved danae/__log__.py at Timestamp: 2019-07-02 21:51:50

--- danae/__log__.py
+++ danae/__log__.py
@@ -233,3 +233,26 @@ Traceback (most recent call last):
     self.aviso = vit.n(porta, TRANCA)
 NameError: name 'porta' is not defined
 '''},
+{'date': 'Tue Jul 02 2019 18:51:49.55 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''sala_do_jogo.A.norte <_spy.vitollino.main.Sala object> <_spy.vitollino.main.Cena object> https://i.imgur.com/aLEjWgB.png
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 153
+    main(grace)
+  module <module> line 149
+    return Livro(bry)
+  module <module> line 88
+    Amilase(self.casa.vit)
+  module amilase.main line 14
+    inv.bota(self.amilase)
+AttributeError: 'module' object has no attribute 'bota'
+'''},
