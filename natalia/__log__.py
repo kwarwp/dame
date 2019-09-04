ba37919c917f4a340df1ea91a1ecf1936e054692
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:18:12 -0300

    Saved natalia/__log__.py at Timestamp: 2019-09-04 13:18:11

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -67,3 +67,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Sep 04 2019 10:18:16.86 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  oi.vai()
+  ^
+IndentationError: unexpected indent
+'''},
