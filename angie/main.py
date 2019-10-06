4ca64562f939f0949d1a17d539cb466d791d5c63
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 15:27:17 -0300

    Saved angie/main.py @2019-10-06 18:27

--- angie/main.py
+++ angie/main.py
@@ -1,19 +1,5 @@
 # dame.angie.main.py
-from _spy.vitollino.main import Cena
-from _spy.vitollino.main import STYLE
-STYLE["width"] = 900
-STYLE["height"] = 700
-CENALIVRO = "https://i.imgur.com/sI177hV.jpg"
 
-class Livro:
-    def __init__(self):
-        self.livro = Cena(img=CENALIVRO)
-        self.livro.vai()
-        
-        
-if __name__ == "__main__":
-    Livro()
-        
         
         
     
