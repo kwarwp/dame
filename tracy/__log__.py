8371ad7b89ef7347dc6445857c9be3f314eb77ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 13:34:07 -0300

    Created tracy/__log__.py @2019-11-01 16:34

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Nov 01 2019 14:34:04.686 GMt-0200 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  soldado.anda()
+  ^
+IndentationError: unexpected indent
+'''},
