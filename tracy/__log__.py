288a3a6e2153f939e05a68e5a8136565fcdc6415
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 13:34:49 -0300

    Saved tracy/__log__.py @2019-11-01 16:34

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Fri Nov 01 2019 14:34:48.274 GMt-0200 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  soldado.anda(guerra)
+  ^
+IndentationError: unexpected indent
+'''},
