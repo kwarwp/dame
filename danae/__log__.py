823f6db317c9ec6c3de1a43a9da1cb038953ae8e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-08 17:00:10 -0300

    Saved danae/__log__.py @2020-08-08 20:00

--- danae/__log__.py
+++ danae/__log__.py
@@ -263,3 +263,30 @@ AttributeError: 'module' object has no attribute 'bota'
                                                                                                                                                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Aug 08 2020 17:00:09.479 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''sala_do_jogo.A.norte <_spy.vitollino.main.Sala object> <_spy.vitollino.main.Cena object> https://i.imgur.com/aLEjWgB.png
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 153
+    main(grace)
+  module <module> line 149
+    return Livro(bry)
+  module <module> line 88
+    Amilase(self.casa.vit)
+  module amilase.main line 12
+    self.amilase = Elemento(AMILASE, tit="Amilase Sintética", drag=True)
+  module elemento.main line 19
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 651
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
