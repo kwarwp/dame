e29cfcf5474b4dc004e6406ca8b277e4788a5def
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-05-17 17:31:37 -0300

    Saved sara/__log__.py @2024-05-17 20:31

--- sara/__log__.py
+++ sara/__log__.py
@@ -22,3 +22,19 @@ ValueError: invalid literal for int() with base 10: 'Coloque um número'
        ^
 SyntaxError: Unbalanced bracket (
 '''},
+{'date': 'Fri May 17 2024 17:31:39.221 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    resposta = lados1 + lados2 + lados3
+TypeError: unsupported operand types for +: 'function' and 'function'
+'''},
