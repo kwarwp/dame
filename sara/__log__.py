cff57235e795ee338256525eaefffb788b15fb4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-05-17 17:45:40 -0300

    Saved sara/__log__.py @2024-05-17 20:45

--- sara/__log__.py
+++ sara/__log__.py
@@ -70,3 +70,19 @@ TypeError: unsupported operand types for +: 'function' and 'function'
     lados1 = int (input('coloque o numero'))
 ValueError: invalid literal for int() with base 10: ''
 '''},
+{'date': 'Fri May 17 2024 17:45:42.108 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    lados2 = int (input('coloque o numero'))
+ValueError: invalid literal for int() with base 10: '65664561234567899907445678456723456781234567812345678901234562345345673456756785678967897890-890678906789567897890'
+'''},
