623db8b3f022bbaa861828623037e89a8bcd0e74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-05 17:40:44 -0300

    Saved danae/__log__.py @2024-09-05 20:40

--- danae/__log__.py
+++ danae/__log__.py
@@ -479,3 +479,30 @@ Traceback (most recent call last):
     self.elt.style.backgroundImage = "url({})".format(value)
 AttributeError: 'Elemento' object has no attribute 'elt'
 '''},
+{'date': 'Thu Sep 05 2024 17:40:44.165 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''sala_do_jogo.A.norte <_spy.vitollino.main.Sala object> <_spy.vitollino.main.Cena object> https://i.imgur.com/aLEjWgB.png
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 153
+    main(grace)
+  module <module> line 149
+    return Livro(bry)
+  module <module> line 88
+    Amilase(self.casa.vit)
+  module amilase.main line 12
+    self.amilase = Elemento(AMILASE, tit="Amilase Sintética", drag=True)
+  module elemento.main line 19
+    self.img, self.title, self.dropper, self.alt = img, tit, drop, alt
+  module _spy.vitollino.main line 668
+    self.elt.style.backgroundImage = "url({})".format(value)
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
